3b4513e6057529a9b6605d1331df4b304beb3d53
Sarah Huchendorf <sarah.coding@outlook.com>
2021-11-11 19:15:54 -0800

    fix bug with screen resolution

---
+++ 2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,33 @@
+// create a constant variable (const)
+const bunny
+// create a variable that can be reassigned (let)
+let wd40 = house;
+
+// create three variables and assign them values of different data types (=)
+let g = "sleep"
+let days = 5
+let pretty = true;
+console.log(typeof pretty);
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+let variable = false;
+variable = 24;
+console.log(typeof variable)
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print
+
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+
+let name = "Sarah";
+let color = "blue";
+let number = 5;
+let sunny = false;
+
+console.log("my name is" + name +", my eyes are" + color + "My fav number is") + number =)
+
+
+
